b01cb219b3114b6053ad492a8ba7e75b8d38cfc7
hershyheilpern <hershyheilpern@gmail.com>
2021-05-03 14:39:41 -0400

    Create asyncloop.js

---
+++ asyncloop.js
@@ -0,0 +1,17 @@
+function asyncloop(...ary) {
+    cb = ary.pop()
+    prams = ary.shift()
+    let i = 0
+    count = ary.length
+    loop(ary,i,count,prams)
+    function loop(ary,i,count,prams) {
+        ary[i](prams,(...res)=>{
+            i++
+            if(i < count){
+                loop(ary,i,count,res)
+            }else{
+                cb(res)
+            }
+        })
+    }
+}
